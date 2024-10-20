4b5cdd9793a1f9693ce7f9cad422e987b5e27a54
EsraaMosaad <70305108+EsraaMosaad@users.noreply.github.com>
2024-10-20 15:18:35 +0300

    add page for all project

---
+++ src/pages/projectsData.js
@@ -0,0 +1,42 @@
+const projectsData = [
+    {
+        types: ["Streamlit","Data processing", "Machine Learning","Docker","MLflow","Deployment"],
+        title: "Bank Customer Churn Prediction - DEPI Final Project",
+        img: "/images/projects/Churn_predction.jpeg",  // No need to import the image
+        link: "https://csj2ctxuwfypzkr4n9xhjc.streamlit.app/",
+        github: "https://github.com/DEPI-project01/DEPI--Project-01"
+    },
+    {
+        types: ["NLP","Recommendation Systems","Data processing"],
+        title: "Job Recommendation and Filtering System",
+        img: "/images/projects/job_recomandation2.png",
+        link: "https://www.canva.com/design/DAFnrTtnyBc/wDG92qtlamPBourWG-04BQ/edit?utm_content=DAFnrTtnyBc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+        github: "https://github.com/EsraaMosaad/GP_Job-Recommendation-and-Filtering_doc/tree/main"
+    },
+    {
+        types: ["Data Visualization","PySpark","Streamlit"],
+        title: "Spotify-Million-Playlist-Dataset-Analysis-and-Visualization",
+        img: "/images/projects/spotify.png",
+        link: "https://github.com/EsraaMosaad/Spotify-Million-Playlist-Dataset-Analysis-and-Visualization/blob/main/app.py",
+        github: "https://github.com/EsraaMosaad/Spotify-Million-Playlist-Dataset-Analysis-and-Visualization"
+    },
+    {
+        types: ["Web Scraping","Data Visualization"],
+        title: "Top IT Companies Worldwide",
+        img: "/images/projects/top_it_company.png",
+        link: "https://github.com/EsraaMosaad/Top-IT-Companies-Worldwide",
+        github: "https://github.com/EsraaMosaad/Top-IT-Companies-Worldwide"
+    },
+    
+    {
+        types: ["Computer Vision"],
+        title: "Semantic Segmentation for Soccer Analytics Using U-Net",
+        img: "/images/projects/football.jpeg",
+        link: "/",
+        github: "https://github.com/EsraaMosaad/ASDC-AI-Data-analysis-internship/blob/main/Task%203/Report.md"
+    },
+    
+    
+];
+
+export default projectsData;
