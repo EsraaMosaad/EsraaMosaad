735cb5b0e77f95df9e8510420f2a91d83e894d61
EsraaMosaad <70305108+EsraaMosaad@users.noreply.github.com>
2024-10-20 16:01:17 +0300

    Remove scribt, Slove erroe

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,60 +1,30 @@
-import Footer from '@/components/Footer';
-import NavBar from '@/components/NavBar';
-import '@/styles/globals.css';
-import { AnimatePresence } from 'framer-motion';
-import { Montserrat } from "next/font/google";
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
+import Footer from '@/components/Footer'
+import NavBar from '@/components/NavBar'
+import '@/styles/globals.css'
+import { AnimatePresence } from 'framer-motion'
+import { Montserrat } from "next/font/google"
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: "--font-mont",
-});
-
+  variable: "--font-mont"
+})
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-
-  // Google Analytics
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag('config', 'G-C4Y4RMRRTD', {
-        page_path: url,
-      });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        
-        {/* Google Analytics Script */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C4Y4RMRRTD"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-C4Y4RMRRTD');
-            `,
-          }}
-        />
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
         <NavBar />
         <AnimatePresence mode='wait'>
-          <Component key={router.asPath} {...pageProps} />
+          <Component key = {router.asPath} {...pageProps} />
         </AnimatePresence>
         <Footer />
       </main>
     </>
-  );
+  )
 }
 
